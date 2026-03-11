d573ba5eba9e3056fc485ad78b906ef33e34213b
Deepanshu Gupta <deepanshugupta0018@gmail.com>
2026-03-11 12:32:55 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,47 +1,52 @@
-import express, { Router } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { createServer } from "http";
 import cors from "cors";
+import { createServer } from "http";
+
 import { initSocket } from "./controllers/socketManager.js";
-import { register } from "./controllers/user.controller.js";
-import { User } from "./models/user.model.js";
-import { login } from "./controllers/user.controller.js";
+import { register, login } from "./controllers/user.controller.js";
 
 dotenv.config();
 
 const app = express();
 const server = createServer(app);
-const io = initSocket(server);
 
-const router = Router();
+// initialize socket
+initSocket(server);
 
 const PORT = process.env.PORT || 8080;
 
-// middleware
+// Middleware 
 app.use(cors());
 app.use(express.json({ limit: "50kb" }));
-app.use(express.urlencoded({ limit: "50kb", extended: true }));
+app.use(express.urlencoded({ extended: true, limit: "50kb" }));
 
-// routes
+
+app.get("/", (req, res) => {
+  res.send("🚀 Backend server is running");
+});
+
+
+const router = express.Router();
+
 router.post("/register", register);
 router.post("/login", login);
 
-// attach router
 app.use("/api", router);
 
-// Server start
+//  Start Server 
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.ATLASDB_URL);
-    console.log("--MongoDB Connected--");
+
+    console.log("✅ MongoDB Connected");
 
     server.listen(PORT, () => {
-      console.log("--Server is running--");
-      console.log(`http://localhost:${PORT}`);
+      console.log(` -- Server running at: http://localhost:${PORT}`);
     });
   } catch (error) {
-    console.error("Server start error:", error);
+    console.error(" !--! Server start error:", error);
   }
 };
 
